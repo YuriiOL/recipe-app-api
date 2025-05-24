00de106e1fa2fd0a679aa128c3ed4e6ca2d7949a
Yurii Oleksovskyi <yuoleksovskyi@echoua.com>
2025-05-24 13:40:33 +0300

    adding test

---
+++ app/app/test.py
@@ -0,0 +1,16 @@
+#Simple tests
+
+from django.test import SimpleTestCase
+
+from app.calc import add, subtract
+
+class SimpleTests(SimpleTestCase):
+    def test_add_numbers(self):
+        res = add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = subtract(5, 6)
+
+        self.assertEqual(res, 1)
